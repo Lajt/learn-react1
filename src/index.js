9f8ad9e345d19a78a9f7fb810ca5651edc5d41e4
Lajt <matti815@gmail.com>
2016-02-10 00:46:20 +0100

    basil fully functionality

--- src/index.js
+++ src/index.js
@@ -19,19 +19,24 @@ class App extends Component{
       selectedVideo: null 
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+
+  }
+  
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term || 'hello world'}, (videos) => {
       // this.setState({videos : videos }); ES6 only works if key and value is the same
       this.setState({ 
         videos: videos, 
         selectedVideo: videos[0] 
       });
     });
-
   }
+  
   render(){
     return(
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
