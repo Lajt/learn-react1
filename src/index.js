6ecea0a0395224273d18da6903f6329aca6ce948
Lajt <matti815@gmail.com>
2016-02-09 21:40:02 +0100

    video list

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import apiKey from './api';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = apiKey;
 console.log(API_KEY);
 
  // Create new component that produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component{
+  constructor(props){
+    super(props);
 
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // this.setState({videos : videos }); ES6 only works if key and value is the same
+      this.setState({ videos });
+    });
+
+  }
+  render(){
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
  // Inject component's generated HTML into DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
