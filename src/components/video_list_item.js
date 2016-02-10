8210fb55ec0d7bc8c99238054c87e0884d167d08
Lajt <matti815@gmail.com>
2016-02-10 01:22:37 +0100

    debounce searching

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 // {video} as argument has same value as const video = props.video;
 const VideoListItem = ({video, onVideoSelect}) => {
-  console.log(video);
   const imageUrl = video.snippet.thumbnails.default.url;
   return (
     <li onClick={ () => onVideoSelect(video) } className="list-group-item">
